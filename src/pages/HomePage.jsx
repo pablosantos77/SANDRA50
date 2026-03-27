d045d59f387a9a324c88eee5b1e6f99095c13c10
Pablo Santos <pablosantosquintas@gmail.com>
2026-03-27 14:23:28 +0100

    style: finalize botanical hero background with visible framing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -54,15 +54,15 @@ export default function HomePage() {
         <div className="absolute inset-0 z-0 pointer-events-none">
           <img
             alt=""
-            className="absolute -top-24 -left-20 w-[450px] opacity-[0.08] rotate-[-15deg] blur-[2px] animate-pulse duration-[8000ms]"
+            className="absolute -top-16 left-1/2 -translate-x-1/2 w-[600px] opacity-[0.25] rotate-[180deg] blur-[0.5px]"
             src="https://lh3.googleusercontent.com/aida-public/AB6AXuCOz07OzXIOKbo89qeG68QyikGF0zF_nmy5QoQ_6FVYwueUsoebajxbVSblE_9HRJjJ_lmHAOxVqdOHBWfqYLHfkOiSnYrCjn7BTOORTsCVO6VHcJOFs1G7LR-j0oma4-EHBVTgdv1Wo3ux--9YA7F31NPZwtkjJ6ZcxldpfaovDbS-XcjSLm7SShtDOhOnWii_swmoVboAl9YPj3cjScr9EjjnkProfdFCVmfNwAltaQMU_Z1ed5jf_Id_Wc6ZA8RwZ3YQt8lM8SPN"
           />
           <img
             alt=""
-            className="absolute -bottom-32 -right-32 w-[550px] opacity-[0.05] rotate-[165deg] blur-[1px]"
+            className="absolute -bottom-24 left-1/2 -translate-x-1/2 w-[700px] opacity-[0.25] rotate-[0deg] blur-[0.5px]"
             src="https://lh3.googleusercontent.com/aida-public/AB6AXuCOz07OzXIOKbo89qeG68QyikGF0zF_nmy5QoQ_6FVYwueUsoebajxbVSblE_9HRJjJ_lmHAOxVqdOHBWfqYLHfkOiSnYrCjn7BTOORTsCVO6VHcJOFs1G7LR-j0oma4-EHBVTgdv1Wo3ux--9YA7F31NPZwtkjJ6ZcxldpfaovDbS-XcjSLm7SShtDOhOnWii_swmoVboAl9YPj3cjScr9EjjnkProfdFCVmfNwAltaQMU_Z1ed5jf_Id_Wc6ZA8RwZ3YQt8lM8SPN"
           />
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-[#fcfcf9]/40 to-[#fcfcf9]"></div>
+          <div className="absolute inset-x-0 bottom-0 h-40 bg-gradient-to-t from-[#fcfcf9] to-transparent"></div>
         </div>
 
         <div className="relative z-10 space-y-6 max-w-2xl mx-auto">
